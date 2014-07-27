c74b19338e407c5abce7df050885c3fd189b49f8
Horacio Chavez <horacio.hch@gmail.com>
2014-07-26 23:02:51 -0600

    User hard coded and required for any admin page

--- lib/app.rb
+++ lib/app.rb
@@ -5,6 +5,18 @@ class PlayerApp < Sinatra::Base
   set :root, '/lib/app'
   set :views, File.dirname(__FILE__) + '/app/views'
 
+  configure do
+    enable :sessions
+  end
+
+  helpers do
+    def authenticate!
+      if params[:user] == "admin" && params[:password] == "admin"
+        session[:user] == "admin"
+      end
+    end
+  end
+
   not_found do
     haml :error
   end
@@ -45,43 +57,66 @@ class PlayerApp < Sinatra::Base
     haml :mvp_club
   end
 
-  get '/admin' do
-    haml :admin
+  get '/login' do
+    haml :login
+  end
+
+  post '/login' do
+    if params[:user] == "ad" && params[:password] == "ad"
+      session[:user] = "player"
+      redirect '/admin/dashboard'
+    else
+      redirect '/'
+    end
+  end
+
+  get '/logout' do
+    haml :index
+    session[:user] = nil
   end
 
   get '/admin/dashboard' do
+    authenticate!
     haml :dashboard
   end
 
   get '/admin/update_home' do
+    authenticate!
     haml :update_home
   end
 
   get '/admin/update_who_we_are' do
+    authenticate!
     haml :update_who_we_are
   end
 
   get '/admin/update_what_we_carry' do
+    authenticate!
     haml :update_what_we_carry
   end
 
   get '/admin/update_what_we_do' do
+    authenticate!
     haml :update_what_we_do
   end
 
   get '/admin/update_outfit_of_the_week' do
+    authenticate!
     haml :update_outfit_of_the_week
   end
 
   get '/admin/update_gift_cards' do
+    authenticate!
     haml :update_gift_cards
   end
 
   get '/admin/update_find_us' do
+    authenticate!
     haml :update_find_us
   end
 
   get '/admin/update_blog' do
+    authenticate!
     haml :update_blog
   end
 
